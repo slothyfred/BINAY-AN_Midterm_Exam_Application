93ce4c72c5851229a59cdbf18786b37d6b04a9f1
N3002 User <N3002user@N3002.uc-bcf.edu.ph>
2024-02-21 09:45:36 +0800

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use app\controller\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/Gadgets',[ProductController::class],'showProducts');
+
+
+
